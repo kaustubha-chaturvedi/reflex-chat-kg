a074e6f0f8e32914651407383641a08e325a0d11
kaustubha-chaturvedi <kaustubhachaturvedi@outlook.com>
2023-10-11 11:21:20 +0530

    fixed fix

--- rxconfig.py
+++ rxconfig.py
@@ -8,7 +8,7 @@ config = WebuiConfig(
     app_name="webui",
     db_url="sqlite:///reflex.db",
     # api_url="https://reflex-chat-w1pj.onrender.com/",
-    api_url="htt://localhost:8000",
+    api_url="http://localhost:8000",
     env=rx.Env.PROD,
     frontend_packages=[
         "react-loading-icons",
